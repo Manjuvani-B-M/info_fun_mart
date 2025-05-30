b02877b2d108aa835162db9d623ec83b77633637
Manjuvani <manjuvanibmanju@gmail.com>
2025-05-30 10:18:56 +0530

    yascommit2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: Process.env.VITE_BASE_PATH || "/info_fun_mart"
+  base:process.env.VITE_BASE_PATH || "/info_fun_mart"
 })
